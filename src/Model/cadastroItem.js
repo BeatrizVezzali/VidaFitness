bfad10141f7306533622fd87c00b7bacb1039585
BeatrizVezzali <beatrizandradevezzali@gmail.com>
2023-03-06 12:04:07 -0300

    Projeto Android

---
+++ src/Model/cadastroItem.js
@@ -0,0 +1,10 @@
+export default class cadastroItem{
+   constructor(Nome, Idade, Peso1, Peso2){
+
+    this.Nome = Nome
+    this.Idade = Idade
+    this.Peso1 = Peso1
+    this.Peso2 = Peso2
+    
+   }
+}
